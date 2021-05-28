e932298d5c6def09cb5d1c1ea5d4372efcafd3a8
Timon Steuer <t.steuer@live.com>
2021-05-28 11:08:47 +0200

    Add check whether file already exists

--- chess-story/__main__.py
+++ chess-story/__main__.py
@@ -19,7 +19,7 @@ from .piece_square_tables import piece_square_tables
 DEFAULT_CONFIG_PATH = r"config.ya?ml"
 
 
-def main(config_path, silent):
+def main(config_path, silent, force):
 
     # Get the config-file.
     config = Config(config_path)
@@ -64,8 +64,17 @@ def main(config_path, silent):
     # Pretty-format the information dict, for easier reading.
     pformat_moves_info = pprint.pformat(moves_info)
 
-    # Export the file.
+    # Check if to be exported file already exists.
     full_export_path = get_full_export_path(load_game, config)
+    if os.path.isfile(full_export_path):
+        if not force:
+            raise OSError(
+                f"The following file already exists: '{full_export_path}'. "
+                "To force overwriting the file, add the 'force' command using "
+                "'-f' or '--force'."
+            )
+
+    # Export the file.
     with open(full_export_path, "w") as exported_file:
         exported_file.writelines(str(game))
         exported_file.write("\n"*2)
@@ -81,6 +90,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, default=None)
     parser.add_argument("-s", "--silent", action="store_true")
+    parser.add_argument("-f", "--force", action="store_true")
     args = parser.parse_args()
 
     if args.config is None:
@@ -97,4 +107,4 @@ if __name__ == "__main__":
     else:
         config_path = args.config
 
-    main(config_path, args.silent)
+    main(config_path, args.silent, args.force)
