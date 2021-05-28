91f1f6854ae27b1a71374d2c64d130731fec90a7
Timon Steuer <t.steuer@live.com>
2021-05-28 08:36:31 +0200

    Refactor full-export-path creation to helpers and add comments

--- chess-story/__main__.py
+++ chess-story/__main__.py
@@ -12,6 +12,7 @@ from .calculations import (
     get_general_info,
     get_development_values
 )
+from .helpers import get_full_export_path
 from .piece_square_tables import piece_square_tables
 
 
@@ -20,19 +21,24 @@ DEFAULT_CONFIG_PATH = r"config.ya?ml"
 
 def main(config_path):
 
+    # Get the config-file.
     config = Config(config_path)
 
+    # Create the path to the game-file specified in the config.
     load_game = config["load_game"]
     pgn_file_path = os.path.join(
         config["pgn_gamefiles_folder_path"],
         load_game
     )
 
+    # Read in the game-file specified in the config.
     with open(pgn_file_path) as pgn_file:
         game = chess.pgn.read_game(pgn_file)
 
+    # Retrieve all moves from game.
     moves = game.mainline_moves()
 
+    # Get all information over the moves from game.
     development_values = get_development_values(moves, piece_square_tables)
     attacks_info = get_attacks_info(moves)
     defenses_info = get_defenses_info(moves)
@@ -52,39 +58,13 @@ def main(config_path):
     }
 
     # Create the folder specified in the config the file will be exported to.
-    export_output_folder_path = config["export_output_folder_path"]
-    os.makedirs(export_output_folder_path, exist_ok=True)
-
-    # Figure out which filename to use for the exported file.
-    export_output_filename = config["export_output_filename"]
-    if export_output_filename is None:
-        export_output_filename = load_game
-
-    # Add the suffix to the filename if necessary.
-    export_output_filename_suffix = config["export_output_filename_suffix"]
-    if export_output_filename_suffix is not None:
-        export_output_filename = (export_output_filename_suffix + ".").join(
-            export_output_filename.split(".")
-        )
-
-    # Add the prefix to the filename if necessary.
-    export_output_filename_prefix = config["export_output_filename_prefix"]
-    if export_output_filename_prefix is not None:
-        export_output_filename = (
-            export_output_filename_prefix
-            + export_output_filename
-        )
-
-    # Create the full export path.
-    full_export_path = os.path.join(
-        export_output_folder_path,
-        export_output_filename
-    )
+    os.makedirs(config["export_output_folder_path"], exist_ok=True)
 
     # Pretty-format the information dict, for easier reading.
     pformat_moves_info = pprint.pformat(moves_info)
 
     # Export the file.
+    full_export_path = get_full_export_path(load_game, config)
     with open(full_export_path, "w") as exported_file:
         exported_file.writelines(str(game))
         exported_file.write("\n"*2)
--- chess-story/helpers.py
+++ chess-story/helpers.py
@@ -1,10 +1,41 @@
 import chess
+import os
 
 
 def get_current_color(board):
     if board.turn:
         return chess.WHITE
     return chess.BLACK
+
+
+def get_full_export_path(load_game, config):
+    # Figure out which filename to use for the exported file.
+    export_output_filename = config["export_output_filename"]
+    if export_output_filename is None:
+        export_output_filename = load_game
+
+    # Add the suffix to the filename if necessary.
+    export_output_filename_suffix = config["export_output_filename_suffix"]
+    if export_output_filename_suffix is not None:
+        export_output_filename = (export_output_filename_suffix + ".").join(
+            export_output_filename.split(".")
+        )
+
+    # Add the prefix to the filename if necessary.
+    export_output_filename_prefix = config["export_output_filename_prefix"]
+    if export_output_filename_prefix is not None:
+        export_output_filename = (
+            export_output_filename_prefix
+            + export_output_filename
+        )
+
+    # Create the full export path.
+    full_export_path = os.path.join(
+        config["export_output_folder_path"],
+        export_output_filename
+    )
+
+    return full_export_path
 
 
 def get_opponents_color(board):
